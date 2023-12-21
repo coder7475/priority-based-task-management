5c01ad7fdf6894636ab51b9a7bd5c0f8a9320753
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 10:14:52 +0600

    feat: add an api to delete a task

--- index.js
+++ index.js
@@ -69,6 +69,16 @@ async function run() {
 
       res.send(result);
     })
+
+    // delete a task
+    app.delete("/api/v1/delete-task/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      
+      const result = await myTasks.deleteOne(filter);
+
+      res.send(result);
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
