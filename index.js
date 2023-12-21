6706425d19e4f3c5d523f6511bd2136851122142
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 08:51:29 +0600

    feat: add cors middleware for express app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // dependencies
 const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // variables
 require('dotenv').config();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`;
 
 // middleware
+app.use(cors());
 app.use(express.json());
 
 // methods
