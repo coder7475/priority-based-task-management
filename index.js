89eb1323a9e0d2444306dc1e4f71538d3412f93a
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 08:47:12 +0600

    feat: add dot env configuration

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// dependencies
 const express = require('express');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// variables
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.DB_USER);
 
+// methods
 app.get("/", (req, res) => {
   res.send("Task Managment Server IS RUNNING!!!")
 })
