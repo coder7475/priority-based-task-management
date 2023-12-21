c89f3112ea2e839646c8959d204422e572588b14
coder7475 <robiulhossain7475@gmail.com>
2023-12-21 08:48:49 +0600

    feat: add express json parser

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER);
+
+// middleware
+app.use(express.json());
 
 // methods
 app.get("/", (req, res) => {
